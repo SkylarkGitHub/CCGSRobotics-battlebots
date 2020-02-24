98c1a4cbe8791d1f9f3c9ab9acdc701cb4ea6da5
Ben Lane <robo.guy@outlook.com>
2020-02-24 10:43:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
+sudo apt-get update
 sudo apt-get install node.js
 sudo apt-get install npm
 npm install express
 sudo -H pip3 install simple-websocket-server
 sudo -H pip3 install pyax12
+sudo apt-get install motion
+sudo modprobe bcm2835-v4l2
